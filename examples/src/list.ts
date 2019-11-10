1b2db6cf0d48faff81c1b5d13be4ec38ef1cea90
Ni55aN <huccah@gmail.com>
2019-11-10 12:44:03 +0200

    use timer instead of setInterval in List example

--- examples/src/list.ts
+++ examples/src/list.ts
@@ -1,19 +1,23 @@
 import { h, $, $$, $for } from 'easyhard';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export default function(container: HTMLElement) {
   const arr = new Array(10).fill(0).map((_, i) => i);
   const list = new $$(arr.map(v => new $(v)));
-
-  setInterval(() => {
+  const randomList = timer(0, 500).pipe(
+    map(() => {
       const i = Math.floor(Math.random() * (list.length - 1));
       const subj = list.value[i];
 
-      subj.next(subj.value + 1);
-  }, 500);
+      return subj.next(subj.value + 1);
+    })
+  );
 
   container.appendChild(
     h('div', {}, 
-        $for(list, (v) => h('div', {}, v))
+      randomList,
+      $for(list, (v) => h('div', {}, v))
     )
   );
 }
