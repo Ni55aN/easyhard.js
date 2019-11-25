99ac18c59e406c6c27527efefb8ab1e5628fd7d8
Ni55aN <huccah@gmail.com>
2019-11-26 01:53:09 +0200

    asapScheduler

--- src/operators.ts
+++ src/operators.ts
@@ -1,9 +1,9 @@
-import { Observable } from "rxjs";
+import { Observable, asapScheduler } from "rxjs";
 import { takeWhile, delay } from "rxjs/operators";
 
 export function untilExist(el: ChildNode | null) {
     return <T>(source: Observable<T>) => {
-      return source.pipe(delay(0), takeWhile<T>(() => {
+      return source.pipe(delay(0, asapScheduler), takeWhile<T>(() => {
         return Boolean(el) && document.body.contains(el);
       }));
     };
