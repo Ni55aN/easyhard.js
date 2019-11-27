717f1f7ccfd3aeecacfa7eb827c146e32cd08c28
Ni55aN <huccah@gmail.com>
2019-11-27 23:51:15 +0200

    update perf: increment an array values

--- examples/src/list/perf.ts
+++ examples/src/list/perf.ts
@@ -1,19 +1,11 @@
 import { h, $, $$, $for } from 'easyhard';
-import { timer } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 function App() {
-  const list = new $$([]);
-  const randomList = timer(0, 16).pipe(
-    map(() => {
-        list.insert(new $(0));
-      
-        return null;
-    })
-  );
+  const list = new $$(new Array(10000).fill(null).map((_, i) => new $(i)));
+  const increment = () => list.value.forEach(v => v.next(v.value + 1));
 
   return h('div', {},
-    randomList,
+    h('button', { click: increment }, 'inc'),
     $for(list, (v) => h('div', {}, v))
   );
 }
