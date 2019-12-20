7aee6a95f184935701e39dac136a077150555061
Ni55aN <huccah@gmail.com>
2019-12-20 15:57:34 +0200

    improve untilExist operator

--- src/operators.ts
+++ src/operators.ts
@@ -1,10 +1,10 @@
-import { Observable, asapScheduler } from "rxjs";
+import { Observable, asapScheduler, MonoTypeOperatorFunction } from "rxjs";
 import { takeWhile, delay } from "rxjs/operators";
 
-export function untilExist(el: ChildNode | null) {
-    return <T>(source: Observable<T>): Observable<T> => {
-      return source.pipe(delay(0, asapScheduler), takeWhile<T>(() => {
-        return Boolean(el) && document.body.contains(el);
-      }));
-    };
-  }
+export function untilExist<T>(el: ChildNode | null, container: Node = document.body): MonoTypeOperatorFunction<T> {
+  return (source: Observable<T>): Observable<T> => {
+    return source.pipe(delay(0, asapScheduler), takeWhile<T>(() => {
+      return Boolean(el) && container.contains(el);
+    }));
+  };
+}
