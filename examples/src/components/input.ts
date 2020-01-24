3ad86decb47b1dec49d5c282d1223f2ef007cbfd
Ni55aN <huccah@gmail.com>
2020-01-24 21:56:04 +0200

    generic type for Input

--- examples/src/components/input.ts
+++ examples/src/components/input.ts
@@ -1,21 +1,24 @@
 import { h, $ } from 'easyhard';
+import { pipe, UnaryFunction } from 'rxjs';
 
-type Props = {
-  value?: $<any>; 
-  model?: $<any>;
+
+type Props<T> = {
+  value?: $<T>; 
+  model?: $<T>;
   type?: 'number' | 'text',
   autofocus?: boolean;
   change?: (v: string) => void,
   events?: {[key: string]: Function}
 };
 
-export function Input(props: Props) {
+export function Input<T>(props: Props<T>) {
     const el = h('input', {
       value: props.value || props.model,
+      type: props.type,
       input: (v: Event) => {
         let val = (v.target as any).value;
+
         if (props.type === 'number') val = +val;
-      
         if (props.model) props.model.next(val);
         if (props.change) props.change(val);
       },
