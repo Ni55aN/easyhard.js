28dde67984494a2d7c112b2e2d510eb1b44d491f
Ni55aN <huccah@gmail.com>
2020-02-01 23:00:54 +0200

    remove Component type

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,9 @@
 import { Observable } from "rxjs";
-import { Fragment } from "./fragment";
 
-export abstract class Component {
-    abstract render(): DomElement;
-}
 export type DomElement = Comment | HTMLElement | Text | null;
 export type Directive = (parent: ChildNode) => DomElement | Fragment;
 export type SimpleType = number | string | boolean | null | void;
-export type Child = DomElement | Component | Fragment | Directive | Observable<SimpleType | DomElement> | SimpleType;
+export type Child = DomElement | Fragment | Directive | Observable<SimpleType | DomElement> | SimpleType;
 
 export type EventAttrs<K extends keyof HTMLElementTagNameMap> = {[key in keyof HTMLElementEventMap]?: Parameters<HTMLElementTagNameMap[K]['addEventListener']>[1]};
 export type PropAttrs<K extends keyof HTMLElementTagNameMap> = {[key in keyof HTMLElementTagNameMap[K]]?: Observable<unknown> | unknown};
