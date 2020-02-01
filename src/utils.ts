f69a5601fa7a426f514c9a61b1e377a3baba40f7
Ni55aN <huccah@gmail.com>
2020-02-01 23:07:10 +0200

    add createFragment to utils instead of Fragment

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import { DomElement } from "./types";
+import { DomElement, Child } from "./types";
+import { appendChild } from "./core";
 
 export function insertAfter(newNode: Node, parentNode: Node, referenceNode: DomElement): void {
     parentNode.insertBefore(newNode, referenceNode && referenceNode.nextSibling);
@@ -6,4 +7,27 @@ export function insertAfter(newNode: Node, parentNode: Node, referenceNode: DomE
 
 export function getNested(nodes: NodeList): Node[] {
   return Array.from(nodes).reduce((acc, node) => ([...acc, node, ...getNested(node.childNodes)]), [] as Node[]);
+}
+
+export function createFragment(): { anchor: Text; edge: DomElement; insert: (item: Child, i?: number) => void; remove: (i: number) => void } {
+  const anchor = document.createTextNode('');
+  const elements: DomElement[] = [];
+
+  return {
+    anchor,
+    get edge(): DomElement {
+      return elements[elements.length - 1] || anchor;
+    },
+    insert(item: Child, i = elements.length): void {
+      const el = appendChild(item, anchor.parentElement as ChildNode, this.edge);
+  
+      elements.splice(i, 0, el);
+    },
+    remove(i: number): void {
+      const exist = elements[i];
+
+      if (exist && 'remove' in exist) exist.remove();
+      elements.splice(i, 1);
+    }
+  }
 }
