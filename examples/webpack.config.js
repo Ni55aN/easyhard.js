c85b436a409e34acc49f1fd89c3d17f4792c0ce0
Ni55aN <huccah@gmail.com>
2020-07-16 00:32:14 +0300

    fix HMR in examples

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
+        options:{	
+          transpileOnly: true	
+        },
         exclude: /node_modules/,
       },
     ],
