8f015aa8d36e0ee7129d4c39f85e9a1f1bab70b3
Ni55aN <huccah@gmail.com>
2020-08-09 17:54:06 +0300

    fix('for' operator): insert initial values

--- packages/core/src/operators/for.ts
+++ packages/core/src/operators/for.ts
@@ -10,12 +10,13 @@ export function $for<T extends any>(array: $$<T>, pipe?: OperatorFunction<T, Dom
   const fragment = createFragment();
 
   merge(array.insert$, array.remove$).pipe(
-    startWith(null),
+    startWith([...array.value]),
     untilExist(fragment.anchor)
   ).subscribe({
     next(args) {
-      if (args === null) {
-        array.value.forEach((item: any) => fragment.insert(pipe ? $(item).pipe(pipe) : item));
+      if (Array.isArray(args)) {
+        fragment.clear()
+        args.forEach((item: any) => fragment.insert(pipe ? $(item).pipe(pipe) : item));
       } else if ('item' in args) {
         fragment.insert(pipe ? $(args.item).pipe(pipe) : args.item, args.i)
       } else {
