ad652e8d2b77d71f0b2403a50b250da383632a6f
Ni55aN <huccah@gmail.com>
2020-08-13 00:09:55 +0300

    feat(array): change$ observable

--- packages/core/src/operators/for.ts
+++ packages/core/src/operators/for.ts
@@ -9,7 +9,7 @@ import { createFragment } from '../utils';
 export function $for<T extends any>(array: $$<T>, pipe?: OperatorFunction<T, DomElement | SimpleType>): DomElement {
   const fragment = createFragment();
 
-  merge(array.insert$, array.remove$).pipe(
+  merge(array.change$).pipe(
     startWith([...array.value]),
     untilExist(fragment.anchor)
   ).subscribe({
@@ -17,9 +17,9 @@ export function $for<T extends any>(array: $$<T>, pipe?: OperatorFunction<T, Dom
       if (Array.isArray(args)) {
         fragment.clear()
         args.forEach((item: any) => fragment.insert(pipe ? $(item).pipe(pipe) : item));
-      } else if ('item' in args) {
+      } else if (args.type === 'insert') {
         fragment.insert(pipe ? $(args.item).pipe(pipe) : args.item, args.i)
-      } else {
+      } else if (args.type === 'remove') {
         fragment.remove(args.i);
       }
     },
--- packages/core/src/structures/array.ts
+++ packages/core/src/structures/array.ts
@@ -2,10 +2,11 @@ import { Subject, merge, Observable, of } from "rxjs";
 import { mergeMap, map } from "rxjs/operators";
 import { $ } from './value';
 
+type Change<T extends 'insert' | 'remove'> = { item: any; i: number; type: T }
+
 export type $$<T> = {
   value$: $<T[]>;
-  insert$: Subject<{ item: any; i: number }>;
-  remove$: Subject<{ i: number }>;
+  change$: Subject<Change<'insert'> | Change<'remove'>>;
   value: T[];
   get(i: number): Observable<T>;
   insert(item: T, i?: number): void;
@@ -17,29 +18,29 @@ export type $$<T> = {
 }
 
 export const $$ = <T>(array: T[]): $$<T> => {
-  const insert$ = new Subject<{ item: unknown; i: number }>();
-  const remove$ = new Subject<{ i: number }>();
+  const change$ = new Subject<Change<'insert'> | Change<'remove'>>();
   const value$ = $<T[]>(array);
 
   function insert(item: T, i = value$.value.length): void {
     value$.value.splice(i, 0, item);
-    insert$.next({ item, i });
+    change$.next({ item, i, type: 'insert' });
   }
 
   function removeAt(i: number): void {
+    const item = value$.value[i]
+
     value$.value.splice(i, 1);
-    remove$.next({ i });
+    change$.next({ item, i, type: 'remove' });
   }
 
   return {
     value$,
-    insert$,
-    remove$,
+    change$,
     get value(): T[] {
       return value$.value;
     },
     get(i: number): Observable<T> {
-      return merge(value$, insert$, remove$).pipe(
+      return merge(value$, change$).pipe(
         mergeMap(() => {
           const value = value$.value[i];
           return value instanceof Observable ? value : of(value)
@@ -58,12 +59,11 @@ export const $$ = <T>(array: T[]): $$<T> => {
     removeAt,
     clear(): void {
       for (let i = value$.value.length - 1; i >=0; i--) {
-        value$.value.splice(i, 1);
-        remove$.next({ i });
+        removeAt(i)
       }
     },
     get length(): Observable<number> {
-      return merge(value$, insert$, remove$).pipe(
+      return merge(value$, change$).pipe(
         map(() => value$.value.length)
       );
     }
