a2634682eea652b36addc6230beb17f682980af3
Ni55aN <huccah@gmail.com>
2020-11-10 01:51:32 +0200

    test(coverage): collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ['core-js'],
-  // collectCoverage: true,
-  // collectCoverageFrom: [
-  //   'packages/*/src/**.ts'
-  // ],
-  // coverageReporters: ['lcov', 'html']
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'packages/*/src/**.ts'
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 };
