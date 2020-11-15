d9942384de5a7528cdaf23a99e844b90dfa42117
Ni55aN <huccah@gmail.com>
2020-11-15 23:04:27 +0200

    fix(hooks): delete lifecycle hooks without element param

--- packages/core/src/hooks/lifecycle.ts
+++ packages/core/src/hooks/lifecycle.ts
@@ -1,28 +1,13 @@
-import { Observable } from 'rxjs'
 import { first } from 'rxjs/operators'
 import { untilExist } from '../operators'
 import { $ } from '../structures/value'
 
-type Cb = () => void
+type Callback = () => void
 
-const onMountElement = (el: ChildNode, cb: Cb) => $('').pipe(untilExist(el), first()).subscribe(cb)
-const onMountInjected = (cb: Cb) => new Observable<null>(() => cb()) // FIXME
-
-export function onMount(...args: Parameters<typeof onMountElement>): void;
-export function onMount(...args: Parameters<typeof onMountInjected>): Observable<null>
-export function onMount(...args: Parameters<typeof onMountElement> | Parameters<typeof onMountInjected>): Observable<null> | void {
-  const [el, cb] = args
-  if (typeof el === 'function') return onMountInjected(el)
-  else onMountElement(el, cb as Cb)
+export function onMount(el: ChildNode, callback: Callback): void {
+  $('').pipe(untilExist(el), first()).subscribe({ next: callback })
 }
 
-const onDestroyElement = (el: ChildNode, cb: Cb) => $('').pipe(untilExist(el)).subscribe({ complete: cb })
-const onDestroyInjected = (cb: Cb) => new Observable<null>(() => cb)
-
-export function onDestroy(...args: Parameters<typeof onDestroyElement>): void;
-export function onDestroy(...args: Parameters<typeof onDestroyInjected>): Observable<null>
-export function onDestroy(...args: Parameters<typeof onDestroyElement> | Parameters<typeof onDestroyInjected>): Observable<null> | void {
-  const [el, cb] = args
-  if (typeof el === 'function') return onDestroyInjected(el)
-  else onDestroyElement(el, cb as Cb)
+export function onDestroy(el: ChildNode, callback: Callback): void {
+  $('').pipe(untilExist(el)).subscribe({ complete: callback })
 }
--- packages/core/test/hooks.test.ts
+++ packages/core/test/hooks.test.ts
@@ -18,33 +18,10 @@ describe('hooks', () => {
     expect(fn).toBeCalled()
   })
 
-  // it('onMount - as child', async () => { // FIXME
-  //   const fn = jest.fn()
-  //   const div = h('div', {}, onMount(fn))
-
-  //   expect(fn).not.toBeCalled()
-  //   document.body.appendChild(div)
-  //   expect(fn).not.toBeCalled()
-  //   await waitAnimationFrame()
-  //   expect(fn).toBeCalled()
-  // })
-
   it('onDestroy - any html element', async () => {
     const fn = jest.fn()
     const div = h('div', {})
     onDestroy(div, fn)
-
-    document.body.appendChild(div)
-    await waitAnimationFrame()
-    expect(fn).not.toBeCalled()
-    document.body.removeChild(div)
-    await waitAnimationFrame()
-    expect(fn).toBeCalled()
-  })
-
-  it('onDestroy - as child', async () => {
-    const fn = jest.fn()
-    const div = h('div', {}, onDestroy(fn))
 
     document.body.appendChild(div)
     await waitAnimationFrame()
