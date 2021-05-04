1576e15e653bc4af46e5ade70aee4a12c21eabd3
Ni55aN <huccah@gmail.com>
2021-05-04 23:50:16 +0300

    fix(examples): replace px with unit function

--- examples/client/list/css.ts
+++ examples/client/list/css.ts
@@ -1,5 +1,5 @@
 import { h } from 'easyhard'
-import { css, injectStyles, StyleDeclaration, px } from 'easyhard-styles'
+import { css, injectStyles, StyleDeclaration, unit } from 'easyhard-styles'
 import { Observable, timer } from 'rxjs'
 import { map } from 'rxjs/operators'
 
@@ -16,14 +16,14 @@ const staticCss = css({
     ':hover': {
         borderColor: timer(0, 500).pipe(map(t => t%2?'red':'blue')),
         borderStyle: 'solid',
-        borderWidth: px(4),
+        borderWidth: unit('px')(4),
         ':focus': {
             background: 'black'
         }
     },
     '@media': {
         query: {
-            minWidth: timer(0, 500).pipe(map(v => px(v+500)))
+            minWidth: timer(0, 500).pipe(map(v => unit('px')(v+500)))
         },
         color: 'green'
     }
@@ -32,7 +32,7 @@ const staticCss = css({
 function App(): HTMLElement {
     const color = timer(0, 1000).pipe(map(v => v % 2 === 0 ? 'red' : 'yellow'))
 
-    return h('div', {}, 
+    return h('div', {},
         h('button', {}, injectStyles(colorTransitionStyle({ color })), injectStyles(staticCss), 'Dynamic'),
         h('button', { className: staticCss.className }, 'Static')
     )
