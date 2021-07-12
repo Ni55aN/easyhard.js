5f4d3ef710180fb28a7bbe71c95911b3e45afa96
Ni55aN <huccah@gmail.com>
2021-07-13 00:04:42 +0300

    feat(examples): update server

--- examples/server/basic.ts
+++ examples/server/basic.ts
@@ -1,55 +1,54 @@
 import { $$ } from 'easyhard-common'
 import fs from 'fs'
 import { easyhardServer, SetCookie, writeFile } from 'easyhard-server'
-import { interval, of, throwError } from 'rxjs'
-import { concatMap, map, scan, take } from 'rxjs/operators'
+import { defer, interval, of, throwError } from 'rxjs'
+import { concatMap, map, mergeMap, scan, take } from 'rxjs/operators'
 import { Actions } from '../shared'
 
 export default easyhardServer<Actions>({
-  getData() {
-    return interval(1000).pipe(
-      take(10),
-      map(count => ({ count }))
-    )
-  },
-  getArray() {
+  getData: interval(1000).pipe(
+    take(10),
+    map(count => ({ count }))
+  ),
+  getArray: defer(() => {
     const array = $$([1,2,3])
 
     setTimeout(() => array.insert(4), 3000)
     setTimeout(() => array.remove(2), 4000)
     return array
-  },
-  getDataWithParams(payload) {
+  }),
+  getDataWithParams: mergeMap((payload) => {
     return interval(500).pipe(
       take(14),
       map(count => ({ count: String(count) + '|' + String(payload.num) }))
     )
-  },
-  getDataError() {
-    return interval(1000).pipe(
-      take(10),
-      concatMap(n => {
-        return n > 3 ? throwError(new Error('more then 3')) : Promise.resolve(n)
-      }),
-      map(count => ({ count }))
-    )
-  },
-  uploadFile(params) {
+  }),
+  getDataError: interval(1000).pipe(
+    take(10),
+    concatMap(n => {
+      return n > 3 ? throwError(new Error('more then 3')) : Promise.resolve(n)
+    }),
+    map(count => ({ count }))
+  ),
+  uploadFile: mergeMap((params) => {
+    params.file.pipe(scan((acc, item: Buffer) => acc + item.length, 0)).subscribe(value => console.log(`Loaded ${value} bytes of second stream`))
     return params.file.pipe(
       writeFile(() => fs.createWriteStream(params.name)),
       scan((acc, buffer) => acc + buffer.length, 0),
       map(loaded => ({ progress: loaded / params.size }))
     )
-  },
-  sendCookie(params) {
+  }),
+  sendCookie: mergeMap((params) => {
     return params.value.pipe( map(value => ({ value, ok: true,  })))
-  },
-  setCookie() {
-    return of({
+  }),
+  setCookie: of(null).pipe(map(() => ({
       newCookie: new SetCookie('test-new', new Date().toISOString(), { path: '/test' }),
-      newCookie2: new SetCookie('test-new2', new Date().toISOString(), { path: '/', httpOnly: true }) })
-  },
-  getDate(params) {
-    return interval(500).pipe(map(() => ({ date: params.date })))
-  }
+      newCookie2: new SetCookie('test-new2', new Date().toISOString(), { path: '/', httpOnly: true })
+  }))),
+  getDate: mergeMap((params) => {
+    return interval(500).pipe(map(() => ({ date: params.date, date2: new Date() })))
+  }),
+  passObservable: mergeMap((params) => {
+    return params.value.pipe(map(value => ({ value })))
+  })
 })
