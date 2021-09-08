b55f74b693af64ba5e23516609ff7543a86ed0db
Ni55aN <huccah@gmail.com>
2021-09-08 22:59:48 +0300

    fix(examples): date pipe

--- examples/client/list/client-date.ts
+++ examples/client/list/client-date.ts
@@ -1,4 +1,4 @@
-import { h, onMount } from 'easyhard'
+import { $, h, onMount } from 'easyhard'
 import { easyhardClient } from 'easyhard-client'
 import { map } from 'rxjs/operators'
 import { Actions } from '../../shared'
@@ -6,11 +6,11 @@ import { Actions } from '../../shared'
 const client = easyhardClient<Actions>()
 
 function App() {
-  const response = client.call('getDate', { date: new Date() })
+  const response = $({ date: new Date() })
 
   const el = h('div', {},
-    h('div', {}, response.pipe(map(data => data.date.toLocaleTimeString()))),
-    h('div', {}, response.pipe(map(data => data.date2.toLocaleTimeString())))
+    h('div', {}, response.pipe(client.pipe('getDate'), map(data => data.date.toLocaleTimeString()))),
+    h('div', {}, response.pipe(client.pipe('getDate'), map(data => data.date2.toLocaleTimeString())))
   )
 
   onMount(el, () => client.connect(() => new WebSocket(`ws://${location.host}/api/basic/`), { http: `http://${location.host}/api/basic/` }))
