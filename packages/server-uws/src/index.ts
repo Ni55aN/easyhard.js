f15d3ce6a727132bc3becf0dfcd47bfea4373160
Ni55aN <huccah@gmail.com>
2021-10-23 20:31:03 +0300

    refactor(server-uws): storing adapter in ws object

--- packages/server-uws/src/index.ts
+++ packages/server-uws/src/index.ts
@@ -1,25 +1,23 @@
 import { WebSocketState } from 'easyhard-bridge'
-import { getUID } from 'easyhard-common'
 import { attach, Attachment, Handlers } from 'easyhard-server'
-import { WebSocketBehavior, WebSocket, HttpRequest } from 'uWebSockets.js'
+import { WebSocketBehavior, WebSocket as uWS, HttpRequest } from 'uWebSockets.js'
 import { ConnectionAdapter } from './connection-adapter'
 import { HttpTunnel, useHttp } from './http'
 import { arrayBufferToString } from './utils'
 
 type Request = HttpRequest & { socket: { ip: string }}
-type Props = { open: (ws: WebSocket, attachment: Attachment) => void }
+type UpgradeData = { req: Request }
+type WebSocket = uWS & UpgradeData & { adapter: ConnectionAdapter }
+type Props = { open: (ws: uWS, attachment: Attachment) => void }
 
 export function easyhardServer<T>(actions: Handlers<T, Request>): { attachClient: (props: Props) => WebSocketBehavior, httpTunnel: HttpTunnel } {
     const http = useHttp()
-    const connections = new Map<string, ConnectionAdapter>()
 
     function attachClient(props: Props): WebSocketBehavior {
         return {
             upgrade(res, req, context) {
-                const id = getUID()
-
-                res.upgrade(
-                    { id, req: <Request>{
+                res.upgrade<UpgradeData>(
+                    { req: {
                         ...req,
                         socket: { ip: arrayBufferToString(res.getRemoteAddressAsText()) }
                     }},
@@ -29,29 +27,28 @@ export function easyhardServer<T>(actions: Handlers<T, Request>): { attachClient
                     context
                 )
             },
-            open: (ws: WebSocket) => {
-                const { id, req } = ws
-                const connection = new ConnectionAdapter(ws)
-                const attachment = attach(actions, connection, req, http)
+            open(uws) {
+                const ws = uws as WebSocket
+                const { req } = ws
 
-                connection.readyState = WebSocketState.OPEN
-                connections.set(id, connection)
+                ws.adapter = new ConnectionAdapter(ws)
+                ws.adapter.readyState = WebSocketState.OPEN
+
+                const attachment = attach(actions, ws.adapter, req, http)
+
                 props.open(ws, attachment)
             },
-            message: (ws, message) => {
-                const id: string = ws.id
-                const connection = connections.get(id)
+            message(uws, message) {
+                const ws = uws as WebSocket
                 const data = arrayBufferToString(message)
 
-                connection?.emit('message', { data })
+                ws.adapter.emit('message', { data })
             },
-            close: (ws, code, message) => {
-                const id: string = ws.id
-                const connection = connections.get(id)
-                if (connection) connection.readyState = WebSocketState.CLOSED
+            close(uws, code, message) {
+                const ws = uws as WebSocket
 
-                connection?.emit('close', { code, reson: message, wasClean: true })
-                connections.delete(id)
+                ws.adapter.readyState = WebSocketState.CLOSED
+                ws.adapter.emit('close', { code, reson: message, wasClean: true })
             }
         }
     }
