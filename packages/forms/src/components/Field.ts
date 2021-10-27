45b7e62d0c1c09106cd8cdcb1cf4ffcde263c3f1
Ni55aN <huccah@gmail.com>
2021-10-27 19:30:41 +0300

    feat(forms): init pipe

--- packages/forms/src/components/Field.ts
+++ packages/forms/src/components/Field.ts
@@ -1,19 +1,22 @@
 import { h, $, DomElement } from 'easyhard'
-import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { Observable, of, OperatorFunction } from 'rxjs'
+import { map, mapTo } from 'rxjs/operators'
 import { FieldInput, InputParams } from './types'
 
 export interface FieldParams<T> extends InputParams<T>  {
   type: FieldInput<T>
   validations?: Observable<(string | boolean)[]>
+  init?: OperatorFunction<ReturnType<FieldInput<T>>, unknown>
 }
 
 export function Field<T>(label: string | Observable<string>, value: $<T>, props: FieldParams<T>): DomElement {
-  const { type, validations, ...params } = props
+  const { type, validations, init, ...params } = props
+  const element = type(value, params)
 
   return h('p', {},
     h('div', {}, label),
-    type(value, params),
+    element,
+    init && of(element).pipe(init, mapTo(null)),
     validations ? validations.pipe(map(validation =>
       h('div', { style: 'color: red' }, validation.map(result => h('div', {}, result === true ? null : result)))
     )) : null
