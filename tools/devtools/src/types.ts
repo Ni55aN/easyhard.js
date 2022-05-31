7989240696fed682ea6ba12eca99d5bd7b39985f
Ni55aN <huccah@gmail.com>
2022-05-31 23:17:42 +0300

    fix(devtools): fix typing

--- tools/devtools/src/types.ts
+++ tools/devtools/src/types.ts
@@ -1,1 +1,14 @@
-export type Services = 'easyhard-devtools' | 'easyhard-content'
+
+
+export type GraphNode = {
+  id: string
+  type: 'eh-text' | 'text' | 'node' | 'eh-node' | 'observable'
+  label: string | null
+}
+export type GraphEdge = { id: string, source: string, target: string }
+export type Graph = { nodes: GraphNode[], edges: GraphEdge[] }
+
+export type Services = {
+  'easyhard-devtools': { type: 'GRAPH', data: Graph }
+  'easyhard-content':  { type: 'GET_GRAPH' }
+}
