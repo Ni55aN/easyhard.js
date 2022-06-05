74a87c17e58587d7ee58f1bb5f9048424b0d6f91
Ni55aN <huccah@gmail.com>
2022-06-05 20:57:56 +0300

    refactor(debug): __debug assignment

--- packages/debug/src/utils.ts
+++ packages/debug/src/utils.ts
@@ -1,8 +1,27 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getUID } from 'easyhard-common'
 import { Observable, OperatorFunction } from 'rxjs'
 
-export function decorateOperator<Args extends never[], Return, Operator extends (...args: Args) => Return>(operator: Operator): Operator {
+type DebugMeta = { __debug?: { id: string, name: string, parent: MestedDebugObject[] } }
+type MestedDebugObject = DebugObject | MestedDebugObject[]
+type DebugOperator = OperatorFunction<any, any> & DebugMeta
+type DebugObservable = Observable<any> & DebugMeta
+type DebugObject = DebugOperator | DebugObservable
+
+function assignMeta(object: DebugObject, name: string) {
+  if (object.__debug) console.warn('__debug already defined in ', object)
+  if (!object.__debug) {
+    object.__debug = {
+      id: getUID(),
+      name,
+      parent: []
+    }
+  }
+  return object.__debug
+}
+
+export function decorateOperator<Args extends never[], Return extends DebugObject, Operator extends (...args: Args) => Return>(operator: Operator): Operator {
   return <Operator>((...args) => {
     const processedArgs = <Args>args.map((arg: any) => {
       if (typeof arg == 'function') {
@@ -10,7 +29,7 @@ export function decorateOperator<Args extends never[], Return, Operator extends 
         return (...params: any[]) => {
           const result = arg(...params)
           if (result instanceof Observable) {
-            (op as any).__debug.parent.push(result)
+            debug.parent.push(result)
           }
           return result
         }
@@ -18,16 +37,9 @@ export function decorateOperator<Args extends never[], Return, Operator extends 
       return arg
     })
     const op = operator(...processedArgs)
+    const debug = assignMeta(op, operator.name)
 
-    if ((op as any).__debug) console.warn('__debug already defined')
-    if (!(op as any).__debug) {
-      (op as any).__debug = {
-        id: getUID(),
-        name: operator.name,
-        parent: []
-      }
-    }
-    (op as any).__debug.parent.push(...processedArgs.filter((a: any) => a instanceof Observable))
+    debug.parent.push(...processedArgs.filter(isDebugLike))
 
     return op
   })
@@ -35,22 +47,17 @@ export function decorateOperator<Args extends never[], Return, Operator extends 
 
 export function decoratePipe(context: any, pipe: any) {
   return (...operations: OperatorFunction<any, any>[]): Observable<any> => {
-    return pipe.apply(context, operations.map(op => {
+    return pipe.apply(context, operations.map((op: DebugOperator) => {
       return (source: Observable<any>) => {
         const res = op(source)
 
-        if (!(op as any).__debug) {
+        if (!op.__debug) {
           throw new Error('operator should have __debug property')
         }
-        if ((res as any).__debug) console.warn('__debug already defined')
-        if (!(res as any).__debug) {
-          (res as any).__debug = {
-            id: getUID(),
-            name: (op as any).__debug.name,
-            parent: []
-          }
-        }
-        (res as any).__debug.parent.push(source, (op as any).__debug.parent ? (op as any).__debug.parent : [])
+        const debug = assignMeta(res, op.__debug.name)
+
+        debug.parent.push(source, op.__debug.parent ? op.__debug.parent : []) // TODO condition
+
         return res
       }
     }))
@@ -59,21 +66,31 @@ export function decoratePipe(context: any, pipe: any) {
 
 export function decorateObservableFactory<Ob extends (...args: any[]) => Observable<any>>(factory: Ob): Ob {
   return <Ob>((...args) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const ob = factory(...args)
-
-    if ((ob as any).__debug) console.warn('__debug already defined')
-    if (!(ob as any).__debug) {
-      (ob as any).__debug = {
-        id: getUID(),
-        name: factory.name,
-        parent: []
+    const processedArgs = args.map((arg: any) => {
+      if (typeof arg == 'function') {
+        return (...params: any[]) => {
+          const result = arg(...params)
+          if (result instanceof Observable) {
+            debug.parent.push(result)
+          }
+          return result
+        }
       }
-    }
-    (ob as any).__debug.parent.push(...args.filter(o => o instanceof Observable || (typeof o === 'object' && 'nodeType' in o)))
+      return arg
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const ob = factory(...processedArgs)
+    const debug = assignMeta(ob, factory.name)
+    const parents: DebugObject[] = processedArgs.filter(isDebugLike)
+
+    debug.parent.push(...parents)
 
     ob.pipe = decoratePipe(ob, ob.pipe)
 
     return ob
   })
+}
+
+function isDebugLike(object: DebugObject) {
+  return '__debug' in object
 }
