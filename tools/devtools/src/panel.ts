d32cc0feef2066305ed659cbe4bf320017c42fdc
Ni55aN <huccah@gmail.com>
2022-06-16 23:48:11 +0300

    fix(devtools): promise on layout stop

--- tools/devtools/src/panel.ts
+++ tools/devtools/src/panel.ts
@@ -22,28 +22,37 @@ connection.addListener(async message => {
       ...message.data.edges.map(data => ({ group: 'edges' as ElementGroup, data }))
     ])
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    await cy.elements().makeLayout({
-      name: 'dagre',
-      rankDir: 'LR',
-      ranker: 'tight-tree',
-      spacingFactor: 0.56,
-      minLen(edge: EdgeSingular) {
-        const nodes = edge.connectedNodes().map((n: any) => n.data())
-
-        if ((nodes[0].type === 'observable') !== (nodes[1].type === 'observable')) {
-          return 6
-        }
-        if (edge.data('type') === 'argument') {
-          return 2
-        }
-
-        return 1
-      },
-      animate: true
-    } as any).run().promiseOn('layoutstop')
+    await layout(true)
   }
 })
+
+async function layout(fit = false) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const layoutInstance = cy.elements().makeLayout({
+    name: 'dagre',
+    rankDir: 'LR',
+    ranker: 'tight-tree',
+    fit,
+    spacingFactor: 0.56,
+    minLen(edge: EdgeSingular) {
+      const nodes = edge.connectedNodes().map((n: any) => n.data())
+
+      if ((nodes[0].type === 'observable') !== (nodes[1].type === 'observable')) {
+        return 6
+      }
+      if (edge.data('type') === 'argument') {
+        return 2
+      }
+
+      return 1
+    }
+  } as any)
+
+  const onStop = layoutInstance.promiseOn('layoutstop')
+
+  layoutInstance.run()
+  await onStop
+}
 
 const onClick = tap(() => {
   connection.postMessage('easyhard-content', { type: 'GET_GRAPH' })
