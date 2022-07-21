d1b411d39bdaca9dd61d09bf03485580cd169a09
Ni55aN <huccah@gmail.com>
2022-07-21 22:47:16 +0300

    fix(debug): support Worker

--- packages/debug/src/setup.ts
+++ packages/debug/src/setup.ts
@@ -1,3 +1,12 @@
-(window as any).__easyhardDebug = true
+function getGlobal() {
+  if (typeof self !== 'undefined') { return self }
+  if (typeof window !== 'undefined') { return window }
+  if (typeof global !== 'undefined') { return global }
+  throw new Error('unable to locate global object')
+}
+
+const debugWindow = <{ __easyhardDebug?: boolean }>getGlobal()
+
+debugWindow.__easyhardDebug = true
 
 console.debug('Easyhard.js debug mode is enabled')
