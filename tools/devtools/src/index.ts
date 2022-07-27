d4a8aaae4a870ef72f9ba37729988a44cddcdde6
Ni55aN <huccah@gmail.com>
2022-07-27 10:56:20 +0300

    feat(devtools): show tab if __easyhardDebug is specified

--- tools/devtools/src/index.ts
+++ tools/devtools/src/index.ts
@@ -1,1 +1,9 @@
-chrome.devtools.panels.create('Easyhard.js', '', '/panel.html')
+chrome.devtools.inspectedWindow.eval<boolean>('Boolean(window.__easyhardDebug)', (debugMode, exceptionInfo) => {
+  if (exceptionInfo) {
+    chrome.devtools.inspectedWindow.eval(`console.error('[Easyhard.js DevTools]', ${JSON.stringify(exceptionInfo.value)})`)
+    return
+  }
+  if (debugMode) {
+    chrome.devtools.panels.create('Easyhard.js', '', '/panel.html')
+  }
+})
