c0d7c10a6027997e2827d49c1ba502a2f0dfd2d4
Ni55aN <huccah@gmail.com>
2022-08-14 21:07:02 +0300

    feat(editor): import raw

--- tools/editor/webpack.config.js
+++ tools/editor/webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source'
+      }
     ],
   },
   resolve: {
