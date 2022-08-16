b042abaaeafa38e2c1360dfd28c4eaf69a3c7f35
Ni55aN <huccah@gmail.com>
2022-08-16 21:31:00 +0300

    refactor(editor): remove rete.js view

--- tools/editor/src/view/components/base.ts
+++
@@ -1,9 +0,0 @@
-
-import { Component as ReteComponent } from 'rete'
-
-export abstract class Component extends ReteComponent {
-    scope: Scope = 'any'
-}
-export type Scope = 'any' | 'root' | (new (...args: any[]) => Component)
-
-
--- tools/editor/src/view/controls/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './button'
-export * from './text'
-export * from './nested'
