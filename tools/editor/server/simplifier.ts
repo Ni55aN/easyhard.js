1abd7c06a2311e4329e2577e3540a127304b6b77
Ni55aN <huccah@gmail.com>
2022-08-28 22:48:43 +0300

    fix(editor): simplify of reusable operators

--- tools/editor/server/simplifier.ts
+++ tools/editor/server/simplifier.ts
@@ -1,43 +1,69 @@
-import { Core } from 'cytoscape'
+import { Core, EdgeSingular, NodeSingular } from 'cytoscape'
+import { getUID } from 'easyhard-common'
 import { pick } from 'lodash'
 
+
 export function simplify(cy: Core) {
+  cy.nodes()
+    .filter(node => node.data('typingKind') === 'OperatorFactory')
+    .forEach(source => {
+      source.outgoers('edge')
+        .filter((edge: EdgeSingular) => edge.data('label') === 'function' && edge.target().data('type') === 'Call')
+        .forEach(edge => {
+          const target = edge.target()
+          const sourceData = pick(source.data(), ['identifier', 'module', 'type'])
+          const targetData = pick(target.data(), ['type', 'parent'])
+
+          target.data({
+            sourceData,
+            targetData,
+            label: source.data('identifier'),
+            identifier: source.data('identifier'),
+            type: 'RxJS'
+          })
+          cy.remove(source)
+        })
+    })
+
+
+  cy.nodes()
+    .filter(node => node.data('typingKind') === 'Operator')
+    .forEach(source => {
+      const incomers = source.incomers('edge')
+      const outgoers = source.outgoers('edge')
+
+      const outgoersCall = outgoers
+        .filter((edge: EdgeSingular) => edge.data('label') === 'function' && edge.target().data('type') === 'Call')
+
+      outgoersCall
+        .forEach(edge => {
+          const target = edge.target()
+          const sourceData = pick(source.data(), ['identifier', 'module', 'type'])
+          const targetData = pick(target.data(), ['type', 'parent'])
+
+          target.data({
+            sourceData,
+            targetData,
+            label: source.data('identifier') || source.data('label'),
+            type: 'RxJS'
+          })
+          target.incomers('edge').forEach(edge => {
+            if (edge.data('label') === 'argument 0') {
+              edge.data('label', '')
+            }
+
+          })
+          incomers.forEach(sourceIncomingEdge => {
+            cy.add({ group: 'edges', data: { ...sourceIncomingEdge.data(), id: getUID(), target: target.data('id') }})
+          })
+        })
+
+      if (outgoers.size() === outgoersCall.size())  cy.remove(source)
+    })
+
   for (const edge of cy.edges().toArray()) {
     const source = edge.source()
     const target = edge.target()
-
-    if (source.data('typingKind') === 'OperatorFactory' && target.data('type') === 'Call') {
-      const sourceData = pick(source.data(), ['identifier', 'module', 'type'])
-      const targetData = pick(target.data(), ['type', 'parent'])
-
-      target.data({
-        sourceData,
-        targetData,
-        label: source.data('identifier'),
-        identifier: source.data('identifier'),
-        type: 'RxJS'
-      })
-      cy.remove(source)
-    }
-    if (source.data('typingKind') === 'Operator' && edge.data('label') === 'function' && target.data('type') === 'Call') {
-      const sourceData = pick(source.data(), ['identifier', 'module', 'type'])
-      const targetData = pick(target.data(), ['type', 'parent'])
-
-      source.incomers('edge').forEach(sourceIncomingEdge => {
-        cy.remove(sourceIncomingEdge)
-        cy.add({ group: 'edges', data: { ...sourceIncomingEdge.data(), target: target.data('id') }})
-      })
-
-      target.data({
-        sourceData,
-        targetData,
-        label: source.data('identifier') || source.data('property'),
-        identifier: source.data('identifier'),
-        property: source.data('property'),
-        type: 'RxJS'
-      })
-      cy.remove(source)
-    }
 
     if (source.data('typingKind') === 'Builtin' && target.data('type') === 'Member') {
       const sourceData = pick(source.data(), ['identifier', 'module', 'type'])
