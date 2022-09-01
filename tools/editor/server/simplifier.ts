850a760e9a79ae5a04a6766055a7671648a0f665
Ni55aN <huccah@gmail.com>
2022-09-01 22:19:52 +0300

    feat(editor): simplify easyhard "h"

--- tools/editor/server/simplifier.ts
+++ tools/editor/server/simplifier.ts
@@ -61,6 +61,52 @@ export function simplify(cy: Core) {
       if (outgoers.size() === outgoersCall.size())  cy.remove(source)
     })
 
+
+  cy.nodes()
+    .filter(node => node.data('type') === 'ImportDeclaration' && node.data('typingKind') === 'EasyhardH')
+    .forEach(source => {
+      source.outgoers('edge')
+        .filter((edge: EdgeSingular) => edge.data('label') === 'function' && edge.target().data('type') === 'Call')
+        .forEach(edge => {
+          const target = edge.target()
+          const argument0 = target.incomers('edge')
+            .filter((edge: EdgeSingular) => edge.data('label') === 'argument 0' && edge.source().data('type') === 'Literal')
+            .source()
+          const argument1 = target.incomers('edge')
+            .filter((edge: EdgeSingular) => edge.data('label') === 'argument 1' && edge.source().data('type') === 'Object')
+            .source()
+
+          target.data({
+            label: argument0?.data('label') || 'element',
+            type: 'EasyhardElement'
+          })
+
+          argument1?.incomers('edge').forEach(argument1Edge => {
+            cy.remove(argument1Edge)
+            cy.add({ group: 'edges', data: { ...argument1Edge.data(), target: target.data('id') }})
+          })
+
+          argument0 && cy.remove(argument0)
+          argument1 && cy.remove(argument1)
+
+          target.incomers('edge')
+            .filter(targetIncomingEdge => targetIncomingEdge.data('label').startsWith('argument'))
+            .forEach(targetIncomingEdge => {
+              const label = targetIncomingEdge.data('label')
+              const index = +label.split(' ')[1]
+
+              if (index === 0) {
+                targetIncomingEdge.data('label', `tag`)
+              } else if (index === 1) {
+                targetIncomingEdge.data('label', `props`)
+              } else {
+                targetIncomingEdge.data('label', `child ${index - 2}`)
+              }
+            })
+          cy.remove(source)
+        })
+    })
+
   for (const edge of cy.edges().toArray()) {
     const source = edge.source()
     const target = edge.target()
