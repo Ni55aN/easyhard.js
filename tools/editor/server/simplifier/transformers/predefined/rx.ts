b206098309a8ff7c967cabc221b4c55e058212a9
Ni55aN <huccah@gmail.com>
2022-09-09 22:04:57 +0300

    feat(editor): recursively simplify rxjs nodes

--- tools/editor/server/simplifier/transformers/predefined/rx.ts
+++ tools/editor/server/simplifier/transformers/predefined/rx.ts
@@ -1,69 +1,57 @@
-import { Core, EdgeSingular } from 'cytoscape'
+import { Core, EdgeSingular, NodeSingular } from 'cytoscape'
 import { getUID } from 'easyhard-common'
 import { pick } from 'lodash'
 import { identifiersToLabel } from '../../utils'
 import { Transformer } from '../interface'
 
 export class RxTransformer implements Transformer {
+  typingKinds = ['OperatorFactory', 'Operator', 'ObservableFactory']
+
+  private convert(cy: Core, source: NodeSingular) {
+    const incomers = source.incomers('edge')
+    const outgoers = source.outgoers('edge')
+
+    const outgoersCall = outgoers
+      .filter((edge: EdgeSingular) => edge.data('label') === 'function' && edge.target().data('type') === 'Call')
+
+    outgoersCall
+      .forEach(edge => {
+        const target = edge.target()
+        const sourceData = pick(source.data(), ['identifiers', 'module', 'type'])
+        const targetData = pick(target.data(), ['type', 'parent'])
+
+        target.data({
+          sourceData,
+          targetData,
+          label: identifiersToLabel(source.data('identifiers')) || source.data('label'),
+          type: 'RxJS'
+        })
+        target.incomers('edge')
+          .filter((edge: EdgeSingular) => edge.data('type') === 'Argument' && edge.source().data('typingKind') === 'Observable')
+          .forEach(edge => {
+            edge.data('label', '')
+            edge.data('type', '')
+          })
+        incomers.forEach(edge => {
+          cy.add({ group: 'edges', data: { ...edge.data(), id: getUID(), target: target.data('id') }})
+        })
+      })
+
+    if (outgoers.size() === outgoersCall.size()) cy.remove(source)
+  }
+
   forward(cy: Core): void {
-    cy.nodes()
-      .filter(node => node.data('type') === 'ImportDeclaration' && node.data('typingKind') === 'OperatorFactory')
-      .forEach(source => {
-        source.outgoers('edge')
-          .filter((edge: EdgeSingular) => edge.data('label') === 'function' && edge.target().data('type') === 'Call')
-          .forEach(edge => {
-            const target = edge.target()
-            const sourceData = pick(source.data(), ['identifiers', 'module', 'type'])
-            const targetData = pick(target.data(), ['type', 'parent'])
+    const candidates = cy.nodes()
+      .filter(node => node.data('type') !== 'Call' && this.typingKinds.includes(node.data('typingKind')))
 
-            target.data({
-              sourceData,
-              targetData,
-              label: identifiersToLabel(source.data('identifiers')),
-              identifiers: source.data('identifiers'),
-              type: 'RxJS'
-            })
-            cy.remove(source)
-          })
-      })
-
-
-    cy.nodes()
-      .filter(node => node.data('typingKind') === 'Operator' || (node.data('type') === 'ImportDeclaration' && node.data('typingKind') === 'ObservableFactory'))
-      .forEach(source => {
-        const incomers = source.incomers('edge')
-        const outgoers = source.outgoers('edge')
-
-        const outgoersCall = outgoers
-          .filter((edge: EdgeSingular) => edge.data('label') === 'function' && edge.target().data('type') === 'Call')
-
-        outgoersCall
-          .forEach(edge => {
-            const target = edge.target()
-            const sourceData = pick(source.data(), ['identifiers', 'module', 'type'])
-            const targetData = pick(target.data(), ['type', 'parent'])
-
-            target.data({
-              sourceData,
-              targetData,
-              label: identifiersToLabel(source.data('identifiers')) || source.data('label'),
-              type: 'RxJS'
-            })
-            target.incomers('edge')
-              .filter(edge => edge.data('type') === 'Argument' && edge.data('label') === 'argument 0')
-              .forEach(edge => {
-                edge.data('label', '')
-                edge.data('type', '')
-              })
-            incomers.forEach(sourceIncomingEdge => {
-              cy.add({ group: 'edges', data: { ...sourceIncomingEdge.data(), id: getUID(), target: target.data('id') }})
-            })
-          })
-
-        if (outgoers.size() === outgoersCall.size())  cy.remove(source)
-      })
-
+    candidates.forEach(source => {
+      this.convert(cy, source)
+    })
+    if (!candidates.empty()) {
+      this.forward(cy)
+    }
   }
   backward(cy: Core): void {
+    cy
   }
 }
