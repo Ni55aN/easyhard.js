cf5f61c323a745ceb16fd5d69c2b333f3685a942
Ni55aN <huccah@gmail.com>
2022-09-10 23:27:59 +0300

    fix(editor): stack overflow on rx simplify

--- tools/editor/server/simplifier/transformers/predefined/rx.ts
+++ tools/editor/server/simplifier/transformers/predefined/rx.ts
@@ -40,7 +40,10 @@ export class RxTransformer implements Transformer {
 
   forward(cy: Core): void {
     const candidates = cy.nodes()
-      .filter(node => ['ImportDeclaration', 'RxJS'].includes(node.data('type')) && this.typingKinds.includes(node.data('typingKind')))
+      .filter(node => ['ImportDeclaration', 'RxJS'].includes(node.data('type'))
+        && this.typingKinds.includes(node.data('typingKind'))
+        && node.outgoers('node[type="Call"]').nonempty()
+      )
 
     candidates.forEach(source => {
       this.convert(cy, source)
